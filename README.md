ab9fef35d2f6a49aff0a2cfb3717e1438b6a66f3
Keith Jones <keith.jones@bjss.com>
2022-05-08 12:02:32 +0100

    Testing with TLS

--- README.md
+++ README.md
@@ -1,8 +1,30 @@
-# Redis POC
+# Redis POC, with TLS
 
 A small proof-of-concept program to familiarise myself with the Redis API.
 
-## Running Redis
+## Environment variables
+
+```
+export REDIS_HOST=<hostname>
+export REDIS_PORT=<port>
+export REDIS_PASSWORD=<password>
+```
+
+## Generating SSL certificates for local development
+
+> If you are on Windows, use WSL. Do not use Git Bash.
+
+Run the following command:
+
+```
+sh gen-test-certs.sh
+```
+
+Add the CA certificate to your list of trusted certificates. The procedure to do this will vary depending on your operating system.
+
+* Windows: https://community.spiceworks.com/how_to/1839-installing-self-signed-ca-certificate-in-windows
+
+## Running the Redis server locally
 
 ```
 docker-compose up -d
@@ -27,4 +49,3 @@ On Linux or macOS you may need to run `dotnet test; stty echo` because reasons.
 ```
 dotnet run --project CLI
 ```
-
--- RedisPOC/RedisWrapper.cs
+++ RedisPOC/RedisWrapper.cs
@@ -7,12 +7,81 @@ public class RedisWrapper: IRedisWrapper
 {
     private static ConnectionMultiplexer? _redis;
     private readonly IDatabase _db;
+
+    // private readonly string _certificateFile = Path.GetFullPath(
+    //     Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "certificate.crt"));
+    
+    private readonly string _redisHost = string.Format("{0}:{1}",
+        Environment.GetEnvironmentVariable("REDIS_HOST"),
+        Environment.GetEnvironmentVariable("REDIS_PORT"));
+
     
     public RedisWrapper()
     {
-        _redis ??= ConnectionMultiplexer.Connect("localhost:6379");
+        // Console.WriteLine($"Certificate file: {_certificateFile}");
+        // var testCert = new X509Certificate2(_certificateFile);
+        // Console.WriteLine($"Serial number: {testCert.SerialNumber}");
+
+        var configurationOptions = new ConfigurationOptions
+        {
+            EndPoints = { _redisHost },
+            Password = Environment.GetEnvironmentVariable("REDIS_PASSWORD"),
+            Ssl = true
+        };
+        // configurationOptions.CertificateSelection += delegate
+        // {
+        //     var cert = new X509Certificate2(_certificateFile);
+        //     return cert;
+        // };
+        // configurationOptions.CertificateValidation += ValidateCertificate;
+        _redis ??= ConnectionMultiplexer.Connect(configurationOptions);
         _db = _redis.GetDatabase();
     }
+    
+    // private static bool ValidateCertificate(
+    //     object sender,
+    //     X509Certificate? certificate,
+    //     X509Chain? chain,
+    //     SslPolicyErrors sslPolicyErrors)
+    // {
+    //     if (sslPolicyErrors == SslPolicyErrors.None)
+    //         return true;
+    //
+    //     Console.WriteLine($"Certificate error: {sslPolicyErrors}");
+    //     var cert = certificate as X509Certificate2;
+    //     if (cert != null)
+    //     {
+    //         Console.WriteLine("Cert: {0}", cert.Subject);
+    //         var extensionNum = 0;
+    //         foreach (var extension in cert.Extensions)
+    //         {
+    //             extensionNum++;
+    //             Console.WriteLine("Extension {0}: {1} = {2}", extensionNum,
+    //                 extension.Oid?.FriendlyName ?? "null",
+    //                 extension.Oid?.Value ?? "null");
+    //         }
+    //     }
+    //     if (chain != null)
+    //     {
+    //         var elementNum = 0;
+    //         foreach (var element in chain.ChainElements)
+    //         {
+    //             elementNum++;
+    //             Console.WriteLine("Chain {0}: {1}", elementNum, element.Certificate.Subject);
+    //             var extensionNum = 0;
+    //             foreach (var extension in element.Certificate.Extensions)
+    //             {
+    //                 extensionNum++;
+    //                 Console.WriteLine("Extension {0}:{1}: {2} = {3}",
+    //                     elementNum, extensionNum,
+    //                     extension.Oid?.FriendlyName ?? "null",
+    //                     extension.Oid?.Value ?? "null");
+    //             }
+    //         }
+    //     }
+    //
+    //     return true;
+    // }
 
     public bool Exists(string key)
     {
